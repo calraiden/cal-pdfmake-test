10e24a7abf1ebc8acefe675ef85ffa820f13100b
calraiden <calraiden@yahoo.com.br>
2017-02-27 17:15:42 -0300

    First commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+import * as pdfmake from 'pdfmake';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  public testPdf() {
+    var dd = { content: 'This is an sample PDF printed with pdfMake' };
+    pdfmake.createPdf(dd).download();
+  }
+}
